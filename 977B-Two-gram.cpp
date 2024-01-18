f3c4c6031c5fb5a89f93e509df50fdfb9816dbb0
aryshende <111281286+aryshende@users.noreply.github.com>
2024-01-18 13:08:47 +0530

    Add files via upload

---
+++ 977B-Two-gram.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int a = 0, b = 0, c = 0, d, i, j, n, m, ck = 0;
+	string arr, brr;
+	char aa, bb;
+	cin >> n;
+	cin >> arr;
+	for (i = 0; i < n; i++)
+	{
+		aa = arr[i];
+		bb = arr[i + 1];
+		ck = 0;
+		for (j = 0; j < n; j++)
+		{
+			if (arr[j] == aa && arr[j + 1] == bb)
+			{
+				ck++;
+			}
+		}
+		if (c < ck)
+		{
+			brr = aa;
+			brr += bb;
+			c = ck;
+		}
+	}
+	cout << brr << endl;
+	return 0;
+}
+
